066bf76daa7c82c271d720cb5990d7b013ddb239
Eduardo Moreno <48771676+edmoreno@users.noreply.github.com>
2026-09-16 02:20:07 +0000

    added markdown image and link extraction functions to helpers.py, along with associated test cases

--- src/helpers.py
+++ src/helpers.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+import re
 
 def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
     new_nodes = []
@@ -6,15 +7,25 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: 
     for old_node in old_nodes:
         if old_node.text_type != TextType.TEXT:
             new_nodes.append(old_node)
-        if delimiter not in old_node.text or old_node.text.count(delimiter) < 2:
+            continue
+        if delimiter not in old_node.text:
+            new_nodes.append(old_node)
+            continue
+        if old_node.text.count(delimiter) % 2 != 0:
             raise Exception(f"Delimiter '{delimiter}' not found in text: {old_node.text}")
 
         split_text = old_node.text.split(delimiter)
 
         for index, text in enumerate(split_text):
-            if index % 2 == 0:
+            if index % 2 == 0 and text:
                 new_nodes.append(TextNode(text, TextType.TEXT))
-            else:
+            elif index % 2 == 1:
                 new_nodes.append(TextNode(text, text_type))
 
     return new_nodes
+
+def extract_markdown_images(text: str) -> list[tuple[str, str]]:
+    return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+
+def extract_markdown_links(text: str) -> list[tuple[str, str]]:
+    return re.findall(r"\[(.*?)\]\((.*?)\)", text)
