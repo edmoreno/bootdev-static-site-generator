140f2261bceb8783a6fe2e0efb2a2aa3862c77c5
Eduardo Moreno <48771676+edmoreno@users.noreply.github.com>
2026-09-18 02:55:52 +0000

    fixed errors

--- src/helpers.py
+++ src/helpers.py
@@ -32,59 +32,53 @@ def extract_markdown_links(text: str) -> list[tuple[str, str]]:
 
 def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
     new_nodes = []
-
     for old_node in old_nodes:
-        image_tuples = extract_markdown_images(old_node.text)
-        original_text = old_node.text
-
         if old_node.text_type != TextType.TEXT:
             new_nodes.append(old_node)
             continue
-        if not image_tuples:
+        original_text = old_node.text
+        images = extract_markdown_images(original_text)
+        if len(images) == 0:
             new_nodes.append(old_node)
             continue
-
-        for image_tuple in image_tuples:
-            sections = original_text.split(f"![{image_tuple[0]}]({image_tuple[1]})", 1)
-            if sections[0]:
+        for image in images:
+            sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, image section not closed")
+            if sections[0] != "":
                 new_nodes.append(TextNode(sections[0], TextType.TEXT))
-            new_nodes.append(TextNode(image_tuple[0], TextType.IMAGE, image_tuple[1]))
+            new_nodes.append(
+                TextNode(
+                    image[0],
+                    TextType.IMAGE,
+                    image[1],
+                )
+            )
             original_text = sections[1]
-
-        if original_text:
+        if original_text != "":
             new_nodes.append(TextNode(original_text, TextType.TEXT))
-
     return new_nodes
+
 
 def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
     new_nodes = []
-
     for old_node in old_nodes:
         if old_node.text_type != TextType.TEXT:
             new_nodes.append(old_node)
             continue
-
-        matches = list(re.finditer(r"(?<!\!)\[(.*?)\]\((.*?)\)", old_node.text))
-        if not matches:
+        original_text = old_node.text
+        links = extract_markdown_links(original_text)
+        if len(links) == 0:
             new_nodes.append(old_node)
             continue
-
-        last_index = 0
-        for match in matches:
-            start, end = match.span()
-            if start > last_index:
-                new_nodes.append(TextNode(old_node.text[last_index:start], TextType.TEXT))
-            new_nodes.append(TextNode(match.group(1), TextType.LINK, match.group(2)))
-            last_index = end
-
-        if last_index < len(old_node.text):
-            new_nodes.append(TextNode(old_node.text[last_index:], TextType.TEXT))
-
+        for link in links:
+            sections = original_text.split(f"[{link[0]}]({link[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, link section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
     return new_nodes
-
-node = TextNode(
-    "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
-    TextType.TEXT,
-)
-new_nodes = split_nodes_image([node])
-print(new_nodes)
